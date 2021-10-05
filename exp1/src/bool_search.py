23dcb47146b684a414e5175643eb7af08c700176
X-XG <934162496@qq.com>
2021-10-05 14:59:35 +0800

    no completed bool_search

--- exp1/src/bool_search.py
+++ exp1/src/bool_search.py
@@ -4,7 +4,25 @@ import re
 class Lex:
     def __init__(self, query:str):
         self.list = query.replace('(', ' ( ').replace(')', ' ) ').split()
+        self.ptr = 0
         print(self.list)
+
+    def GetIdList(self) -> list:
+        if(self.list[self.ptr] == '('):
+            self.ptr += 1
+            self.GetIdList()
+        elif(self.list[self.ptr] ==')'):
+            return
+        else:
+            L1 = GETLIST(self.list[self.ptr])
+            self.ptr += 1
+            if(self.list[self.ptr] == 'AND'):
+                self.ptr += 1
+                L2 = self.GetIdList()
+                return Lex.AND(L1, L2)
+
+    def AND(L1:list, L2:list) -> list:
+
 
 if __name__ == '__main__':
     a = Lex('AA AND  (FFF )SK')
