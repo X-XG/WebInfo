bf3e0a8462fce73325ab12cdf7c473030b3bbe28
ljy0ustc <1501066217@qq.com>
2021-10-14 19:43:14 +0800

    git add semantic_search word2vec_train

--- exp1/src/semantic_search.py
+++ exp1/src/semantic_search.py
@@ -6,7 +6,10 @@ import nltk
 import re
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
-from DocSparse import Doc_Sparse
+from nltk.corpus import wordnet
+from DocSparse import Doc_Sparse as tf_idf_sparse
+from word2vec_train import Doc_Sparse as w2v_sparse
+from gensim.models import word2vec
 
 import numpy as np
 import math
@@ -15,35 +18,73 @@ def takeSecond(elem):
         return elem[1]
 
 class Semantic_Search:
-    def __init__(self, query_str):
+    def __init__(self, query_str, synonym_tag = False,embedding_type = "tf-idf"):
         self.query_str = query_str
+        self.synonym_tag = synonym_tag
+        self.embedding_type = embedding_type
 
-        postinglist_path = '..\\output\\PostingList.json'
-        docmap_path = '..\\output\\docmap.json'
-        wordmap_path = '..\\output\\wordmap.json'
+    def read_file(self):
+        if self.embedding_type == "tf-idf":
+            print("tf-idf")
+            postinglist_path = '..\\output\\PostingList.json'
+            docmap_path = '..\\output\\docmap.json'
+            wordmap_path = '..\\output\\wordmap.json'
 
-        with open(postinglist_path,'r',encoding="utf-8")as fp_postinglist:
-            self.PostingList = json.load(fp_postinglist)
-            self.word_num = self.PostingList[0]
+            with open(postinglist_path,'r',encoding="utf-8")as fp_postinglist:
+                self.PostingList = json.load(fp_postinglist)
+                self.word_num = self.PostingList[0]
 
-        with open(docmap_path,'r',encoding="utf-8")as fp_docmap:
-            self.DocMap = json.load(fp_docmap)
-            self.doc_num = self.DocMap[0]
+            with open(docmap_path,'r',encoding="utf-8")as fp_docmap:
+                self.DocMap = json.load(fp_docmap)
+                self.doc_num = self.DocMap[0]
 
-        with open(wordmap_path,'r',encoding="utf-8")as fp_wordmap:
-            self.WordMap = json.load(fp_wordmap)
+            with open(wordmap_path,'r',encoding="utf-8")as fp_wordmap:
+                self.WordMap = json.load(fp_wordmap)
+
+            self.TF_IDF_array = np.load("..\\output\\TF_IDF.npy")
         
-        self.TF_IDF_array = np.load("..\\output\\TF_IDF.npy")
+        if self.embedding_type == "word2vec":
+            print("word2vec")
+            model_output_path = "..\\output\\word2vec.model"
+            self.model = word2vec.Word2Vec.load(model_output_path)
+
+            docmap_path = '..\\output\\docmap.json'
+            doc_word_list_path = '..\\output\\doc_word_list.json'
+
+            with open(docmap_path,'r',encoding="utf-8")as fp_docmap:
+                self.DocMap = json.load(fp_docmap)
+                self.doc_num = self.DocMap[0]
+
+            with open(doc_word_list_path,'r',encoding="utf-8")as fp_doc_word_list:
+                self.doc_word_list = json.load(fp_doc_word_list)
 
     def sparse_query(self):
-        doc_sparse = Doc_Sparse(self.query_str)
-        self.query_words,self.query_fdist = doc_sparse.sparse()
+        doc_sparse = tf_idf_sparse(self.query_str)
+        query_words,self.query_fdist = doc_sparse.sparse()
+        self.query_freqs = {}
+        for word in query_words:
+            self.query_freqs[word] = self.query_fdist.freq(word)
+        if self.synonym_tag:
+            self.query_words = []
+            for word in query_words:
+                syn_wordlist = wordnet.synsets(word)
+                for syn in syn_wordlist:
+                    for lemma in syn.lemmas():
+                        syn_word = lemma.name()
+                        if(syn_word in self.WordMap):
+                            self.query_words.append(syn_word)
+                            self.query_freqs[syn_word] = self.query_freqs[word]
+            self.query_words += query_words
+            self.query_words = list(set(self.query_words))
+        else:
+            self.query_words = query_words
 
     def tf_idf_cal(self):
         """calculate the tf-idf vector of the query"""
+        """if synonym_tag = True, use synonyms to search"""
         self.TF_IDF = np.zeros(self.word_num + 1)
         for word in self.query_words:
-            freq = self.query_fdist.freq(word)
+            freq = self.query_freqs[word]
             word_id = self.WordMap[word]
             TF = freq
             IDF = math.log(self.doc_num/(len(self.PostingList[word_id]) + 1))
@@ -51,35 +92,42 @@ class Semantic_Search:
 
     def similarity(self):
         """calculate the similarity between the query vector with each document"""
-        self.doc_similarity =[0]
-        for doc_id in range(1, self.doc_num + 1):
-            #cos similarity
-            sim = np.dot(self.TF_IDF_array[doc_id],self.TF_IDF)/(np.linalg.norm(self.TF_IDF_array)*np.linalg.norm(self.TF_IDF))
-            self.doc_similarity.append([doc_id,sim])
+        self.read_file()
+        if self.embedding_type == "tf-idf":
+            self.sparse_query()
+            self.tf_idf_cal()
+            self.doc_similarity =[0]
+            for doc_id in range(1, self.doc_num + 1):
+                #cos similarity
+                sim = np.dot(self.TF_IDF_array[doc_id],self.TF_IDF)/(np.linalg.norm(self.TF_IDF_array)*np.linalg.norm(self.TF_IDF))
+                self.doc_similarity.append([doc_id,sim])
+
+        if self.embedding_type == "word2vec":
+            query_word_sparse = w2v_sparse(self.query_str)
+            query_word = query_word_sparse.sparse()
+            self.doc_similarity =[0]
+            for doc_id in range(1,self.doc_num + 1):
+                doc_word = self.doc_word_list[doc_id - 1]
+                sim = self.model.n_similarity(query_word,doc_word)
+                self.doc_similarity.append([doc_id,sim])
+
 
     def ranking(self):
+        """run this runction"""
         """rank the document with similarity"""
+        """return top10_doc_name"""
         self.similarity()
         #sort
         self.doc_similarity[1:].sort(key=takeSecond, reverse=True)
         #pick the top10
         top10_doc_name = []
-        for i in range(1, 11):
+        for i in range(1, min(self.doc_num,11)):
             doc_id = self.doc_similarity[i][0]
             top10_doc_name.append(self.DocMap[doc_id])
         return top10_doc_name
 
-
-    def semantic_search(self):
-        """run this runction"""
-        """return top10_doc_name"""
-        self.sparse_query()
-        self.tf_idf_cal()
-        res = self.ranking()
-        return res 
-
 #####words query............
-S = Semantic_Search("president high")
-print(S.semantic_search())
+S = Semantic_Search("president high", synonym_tag = True, embedding_type = "word2vec")
+print(S.ranking())
         
     
---
+++ exp1/src/word2vec_train.py
@@ -0,0 +1,99 @@
+import os
+import json
+from gensim.models import word2vec
+import nltk
+import re
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+class Doc_Sparse:
+    def __init__(self,text):
+        self.document = text
+
+    def document_to_sentence(self):
+        # replace '\n' with blank 
+        self.document = re.sub('\n',' ',self.document)
+        if isinstance(self.document,str):
+            self.document = self.document
+        else:
+            raise ValueError('DOcument is not string!')
+        #delete '\n',''\r',''\t','' in the in the begining and end
+        self.document = self.document.strip()
+        sentences = nltk.sent_tokenize(self.document)
+        self.sentences = [sentence.strip() for sentence in sentences]
+
+    def sentence_to_tokenized(self):
+        original_words = []
+        for sentence in self.sentences:
+            original_words += nltk.word_tokenize(sentence)
+        self.original_words=[word.lower() for word in original_words if word.isalpha()]
+
+    def stem_word(self):
+        stemmer = PorterStemmer()
+        self.stemmed_words = [stemmer.stem(word) for word in self.original_words]
+
+    def stop_word(self):
+        stopwords.words('english')
+        clean_words = self.stemmed_words[:]
+        for word in self.stemmed_words:
+            if word in stopwords.words('english'):
+                clean_words.remove(word)
+        self.clean_words = clean_words
+
+    def sparse(self):
+        self.document_to_sentence()
+        self.sentence_to_tokenized()
+        self.stem_word()
+        self.stop_word()
+        return self.clean_words
+
+class Word2Vec_Embedding:
+    def __init__(self):
+        doc_id = 0 
+        self.Doc_Word_List = [0]
+        self.DocMap = [0]
+        rootdir_base = '..\\dataset\\US_Financial_News_Articles\\2018_0'
+        for i in range(1,6):
+            rootdir = rootdir_base + str(i)
+            list = os.listdir(rootdir)
+            for j in range(0,len(list)):
+                path = os.path.join(rootdir,list[j])
+                file_name_split = path.split('\\')
+                file_name = "2018_0"+str(i)+"\\"+file_name_split[-1]
+                if os.path.isfile(path):
+                    #procedures on document
+                    with open(path,'r',encoding="utf-8")as fp:
+                        json_data = json.load(fp)
+                        doc_id += 1
+
+                        #convert document to word
+                        title = json_data["title"]
+                        text = json_data["text"]
+                        doc_sparse = Doc_Sparse(title + text)
+                        self.Doc_Word_List.append(doc_sparse.sparse())
+                        
+                        self.DocMap.append(file_name)
+        self.DocMap[0] = doc_id
+        self.sentences = []
+        for doc_id in range(1,self.DocMap[0]+1):
+            self.sentences.append(self.Doc_Word_List[doc_id])
+
+    def train(self,model_output_path):
+        """run this"""
+        model = word2vec.Word2Vec(sentences=self.sentences,size=100,window=5,min_count=0)
+        model.save(model_output_path)
+
+    def write_to_file(self):
+        jsonDocMap = json.dumps(self.DocMap)
+        fileObject1 = open('..\\output\\DocMap.json', 'w')  
+        fileObject1.write(jsonDocMap)  
+        fileObject1.close()
+
+        jsonDocMap = json.dumps(self.sentences)
+        fileObject2 = open('..\\output\\doc_word_list.json', 'w')  
+        fileObject2.write(jsonDocMap)  
+        fileObject2.close()
+
+w2v = Word2Vec_Embedding()
+w2v.train("..\\output\\word2vec.model")
+w2v.write_to_file()
