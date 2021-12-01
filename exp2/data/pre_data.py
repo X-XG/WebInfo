8006c82b39ca0a5d70437b2fc122833f373dec9b
X-XG <934162496@qq.com>
2021-12-01 21:53:06 +0800

    w2v and transE combination

---
+++ exp2/data/pre_data.py
@@ -0,0 +1,41 @@
+def exchange(data_path):
+    f = open(data_path, 'r')
+    lines = f.readlines()
+    f.close()
+    f = open(data_path, 'w')
+    for line in lines:
+        temp = line.split()
+        f.write(temp[0])
+        f.write('\t')
+        f.write(temp[2])
+        f.write('\t')
+        f.write(temp[1])
+        f.write('\n')
+    f.close()
+
+def data2id(num, data_path):
+    f = open(data_path, 'w')
+    for i in range(0, num):
+        f.write(str(i))
+        f.write('\t')
+        f.write(str(i))
+        f.write('\n')
+    f.close()
+
+if __name__ == '__main__':
+    base_path = './'
+    ent_num = 14541
+    rel_num = 237
+
+    train_path = base_path + 'train.txt'
+    dev_path = base_path + 'dev.txt'
+    test_path = base_path + 'test.txt'
+    exchange(train_path)
+    exchange(dev_path)
+    exchange(test_path)
+
+    entity2id_path = base_path + 'entity2id.txt'
+    relation2id_path = base_path + 'relation2id.txt'
+    data2id(ent_num, entity2id_path)
+    data2id(rel_num, relation2id_path)
+
---
+++ exp2/w2v_transE/w2vgen.py
@@ -0,0 +1,57 @@
+from gensim.models import Word2Vec
+import numpy as np
+import pickle
+
+def w2vModel(data_base):
+    common_texts = []
+    ent_map = {}
+    rel_map = {}
+    f = open(data_base + 'entity_with_text.txt', 'r')
+    lines = f.readlines()
+    for line in lines:
+        temp = line.split('\t')  
+        text = temp[1].split()
+        common_texts.append(text)
+        ent_map[int(temp[0])] = text
+    f.close()
+
+    f = open(data_base + 'relation_with_text.txt', 'r')
+    lines = f.readlines()
+    for line in lines:
+        temp = line.split('\t')  
+        text = temp[1].split()
+        common_texts.append(text)
+        rel_map[int(temp[0])] = text
+    f.close()
+
+    model = Word2Vec(sentences=common_texts, vector_size=100, window=5, min_count=1, workers=4)
+    model.save("word2vec.model")
+
+    with open('ent_map_text'+ '.pkl', 'wb') as f:
+        pickle.dump(ent_map, f)
+    
+    with open('rel_map_text'+ '.pkl', 'wb') as f:
+        pickle.dump(rel_map, f)
+
+
+def ent_rel_sim(ent_num, rel_num):
+    model = Word2Vec.load("word2vec.model")
+    with open('ent_map_text' + '.pkl', 'rb') as f:
+        ent_map = pickle.load(f)
+    with open('rel_map_text' + '.pkl', 'rb') as f:
+        rel_map = pickle.load(f)
+
+    ent_rel_sim = np.zeros((ent_num, rel_num))
+    for i in range(ent_num):
+        if i % 100 == 0:
+            print(i)
+        if i not in ent_map:
+            continue
+        for j in range(rel_num):
+            ent_rel_sim[i][j] = model.wv.n_similarity(ent_map[i], rel_map[j])
+    
+    np.save('ent_rel_sim.npy', ent_rel_sim)
+    
+if __name__  == '__main__':
+    w2vModel('./data/')
+    ent_rel_sim(14541, 237)
